35b3e416912c3fe9a203d40258341a45aa556b3a
mariam-hamdy <mariam.hamdy1215@gmail.com>
2022-08-31 15:02:32 +0200

    implement product-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarSearchComponent } from './shared/navbar-search/navbar-search.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LoginComponent
+    LoginComponent,
+    FooterComponent,
+    NavbarSearchComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
